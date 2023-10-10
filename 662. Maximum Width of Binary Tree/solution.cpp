0eecb05f849afc28a530c961825bfdecbbe30b32
Ayush8165 <thakur.ayush8165@gmail.com>
2023-10-10 10:51:09 +0530

    10/10

--- 662. Maximum Width of Binary Tree/solution.cpp
+++ 662. Maximum Width of Binary Tree/solution.cpp
@@ -42,27 +42,51 @@ TreeNode *buildTree()
     }
 }
 
+// 2i+1 and 2i+2
+
+int widthOfBinaryTree(TreeNode *root)
+{
+    queue<pair<TreeNode *, long long>> que;
+    que.push({root, 1});
+    long long lvlmin = 1;
+    int lft, rht;
+    int ans = 0;
+
+    while (!que.empty())
+    {
+        int count = que.size();
+        lvlmin = que.front().second;
+        for (int i = 0; i < count; i++)
+        {
+            TreeNode *troot = que.front().first;
+            long long tval = que.front().second;
+            que.pop();
+            if (i == 0)
+            {
+                lft = tval;
+            }
+            if (i == count - 1)
+            {
+                rht = tval;
+            }
+            if (troot->left)
+            {
+                que.push({troot->left, 2 * (tval - lvlmin) + 1});
+            }
+            if (troot->right)
+            {
+                que.push({troot->right, 2 * (tval - lvlmin) + 2});
+            }
+        }
+        ans = max(rht - lft + 1, ans);
+    }
+    return ans;
+}
+
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-
-    vector<int> v;
-    v.push_back(-1);
-    v.push_back(-1);
-    v.push_back(-1);
-    v.push_back(1);
-    v.push_back(3);
-    v.push_back(-1);
-    v.push_back(5);
-    v.push_back(-1);
-    v.push_back(-1);
-    v.push_back(-1);
-    
-    for (auto it : v)
-    {
-        cout << it << " ";
-    }
 
     return 0;
 }
